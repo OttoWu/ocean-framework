ccf98a86d9b3a4b432d06d0679d1cf7bcaca2abe
ottowu <270214114@qq.com>
2022-04-27 23:41:15 +0800

    初始化提交，以spring-boot构建项目底座

---
+++ src/main/java/com/ottowu/oceanframework/OceanFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.ottowu.oceanframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ottowu/oceanframework/OceanFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ottowu.oceanframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
